923bbe0d65d40349784e9d6a1f509c88e9d3497c
Minhtran1412 <minhtrn001@gmail.com>
2024-03-16 01:43:20 +0700

    minh
    minh

--- WebApplication1/src/java/Dao/HouseDAO.java
+++ WebApplication1/src/java/Dao/HouseDAO.java
@@ -12,16 +12,21 @@ import java.sql.PreparedStatement;
  * @author acer
  */
 public class HouseDAO extends DBContext{
-    public void updateHouse(int id, String Address, int Size, int bed, int bath, int gara, int price, int providerID, String img, Date CreateDate, String Description){
-        String sql = "update Account_Customer set [customer_name] = ?, [customer_email] = ?, [customer_address]=?,[customer_phone]=?,[customer_dob]=? where [customer_id]=?";
+    public void updateHouse(int id, String Address, int Size, int bed, int bath, int gara, int price, int providerID, String img, Date createDate, String Description){
+        String sql = "update Property set [Address] = ?, [Size] = ?, [bed]=?,[bath]=?,[gara]=?, [price]=?, [providerID]=?, [img]=?, [CreateDate]=?, [Description]=? where [id]=?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
-            st.setString(1, customer_name);
-            st.setString(2, customer_email);
-            st.setString(3, customer_address);
-            st.setString(4, customer_phone);
-            st.setString(5, customer_dob);
-            st.setString(6, customer_id);
+            st.setString(1, Address);
+            st.setInt(2, Size);
+            st.setInt(3, bed);
+            st.setInt(4, bath);
+            st.setInt(5, gara);
+            st.setInt(6, price);
+            st.setInt(7, providerID);
+            st.setString(8, img);
+            st.setDate(9, createDate);
+            st.setString(10, Description);
+            st.setInt(11, id);
             st.executeUpdate();
         } catch (Exception e) {
         }
