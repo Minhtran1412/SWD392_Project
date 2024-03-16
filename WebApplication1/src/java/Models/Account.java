ffd9d1e0c1a9a8bcfcac38807683f02e488ae247
phamhuuvietanh <anhphvhe151407@fpt.edu.vn>
2024-03-17 01:41:56 +0700

    Login

---
+++ WebApplication1/src/java/Models/Account.java
@@ -0,0 +1,151 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+
+import java.util.Date;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author dell
+ */
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class Account {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+    private String phone; 
+    private Date dob;
+    private String avt;
+    private String status;
+    private String role;
+
+//    public Account() {
+//    }
+//
+//    public Account(int id, String name, String username, String password, String email, String address, String phone, Date dob, String avt, String status, String role) {
+//        this.id = id;
+//        this.name = name;
+//        this.username = username;
+//        this.password = password;
+//        this.email = email;
+//        this.address = address;
+//        this.phone = phone;
+//        this.dob = dob;
+//        this.avt = avt;
+//        this.status = status;
+//        this.role = role;
+//    }
+//
+//    public Account(String string, String string0) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public Date getDob() {
+//        return dob;
+//    }
+//
+//    public void setDob(Date dob) {
+//        this.dob = dob;
+//    }
+//
+//    public String getAvt() {
+//        return avt;
+//    }
+//
+//    public void setAvt(String avt) {
+//        this.avt = avt;
+//    }
+//
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Account{" + "id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + ", email=" + email + ", address=" + address + ", phone=" + phone + ", dob=" + dob + ", avt=" + avt + ", status=" + status + ", role=" + role + '}';
+//    }
+    
+}
